17e44b118629a9d0e5cc1c1472cf25285032b785
junwen-shift <junwen.yin@shift-technology.com>
2024-10-29 22:37:23 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -80,7 +80,7 @@ async function analyzeCode(
 
 function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
   return `Your task is to review pull requests. Instructions:
-- Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
+- Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}, without adding any code like \`\`\`json.
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
